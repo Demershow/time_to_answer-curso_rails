b5a586d4d641314d85c479ae1fa2ec822e7df22e
demershow <demersontorres520@gmail.com>
2022-11-21 09:33:51 -0300

    Aula 124: Creating the login amdmin and profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  #http_basic_authenticate_with name: "demerson", password: "123"
   def index
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,13 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  #Devise config
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -29,7 +36,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
