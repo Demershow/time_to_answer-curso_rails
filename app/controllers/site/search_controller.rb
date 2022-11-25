55b4ca15c79551dbfd943d83b4e852eb2ccdb273
demershow <demersontorres520@gmail.com>
2022-11-25 09:01:03 -0300

    Aula 194: Making the search bar work

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,5 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers).where("lower(description) LIKE ?", "%#{params[:term].downcase}%").page(params[:page])
   end
 end
