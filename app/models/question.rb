24b0d2f285044faa4f5fd6b36fe79d6e2919062c
demershow <demersontorres520@gmail.com>
2022-11-25 08:31:56 -0300

    Aula 192: listing the questions and answers on index

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: :all_blank
+  paginates_per 5
 end
