9846addbdd003627069429979b6a859bc59095c3
demershow <demersontorres520@gmail.com>
2022-11-25 11:11:30 -0300

    Aula 204: filtering the questions by subject

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Ultimas perguntas cadastradas..."
+    when 'questions'
+      "Resultados par ao termo \"#{params[:term]}\"..."
+    when 'subject'
+      "Mostrando questões para o assunto\"#{params[:subject]}\"..."
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,13 +4,17 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: :all_blank
   paginates_per 5
 
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject).where(subject_id: subject_id).page(page)
+  }
+
   scope :_search_, ->(page, term){
-    includes(:answers)
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
     .page(page)
   }
 
   scope :last_questions, ->(page) {
-    includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject).order('created_at desc').page(page)
   }
 end
