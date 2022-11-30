5524ec709d4b662445fdd11f8ae8871349bd8d56
demershow <demersontorres520@gmail.com>
2022-11-30 08:26:19 -0300

    Aula 225: Refactoring the UserStatistic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_28_120222) do
+ActiveRecord::Schema.define(version: 2022_11_30_110950) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2022_11_28_120222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "right_questions", default: 0
+    t.integer "wrong_questions", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
