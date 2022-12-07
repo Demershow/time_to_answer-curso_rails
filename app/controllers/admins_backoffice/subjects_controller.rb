ba1cdeb504bdc93059c27ba41b629fe2a5c12df3
demershow <demersontorres520@gmail.com>
2022-12-07 07:53:16 -0300

    Aula 264: answearing the html and json format

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -5,6 +5,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     respond_to do |format|
     format.html {@subjects = Subject.all.order(:description).page(params[:page])}
     format.pdf  {@subjects = Subject.all.order(:description)}
+    format.json {@subjects = Subject.all.order(:description)}
   end
 
   def new
